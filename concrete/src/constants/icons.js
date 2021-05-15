f56c6e6512ee27930b265029f9a0265e1da6793e
fernando <fernando@pop-os.localdomain>
2021-05-15 19:56:56 -0300

    Add de constantes

---
+++ concrete/src/constants/icons.js
@@ -0,0 +1,5 @@
+const URL_ICON = {
+    GIT_HUB: 'https://image.flaticon.com/icons/png/512/25/25231.png', 
+}
+
+export default URL_ICON
---
+++ concrete/src/constants/status-code.js
@@ -0,0 +1,13 @@
+const STATUS_CODE = {
+    200: 'Operação realizada com sucesso!',
+    201: 'Criado com sucesso!',
+    204: 'Nenhum conteúdo encontrado',
+    300: 'Tente novamente!',
+    400: 'Algum dados foi enviado incorreto, tente novamente!',
+    401: 'Você não esta autenticado!',
+    403: 'Você não possui permissão para esta ação!',
+    500: 'Estamos com problemas tente novamente mais tarde!',
+    504: 'Tempo de resposta esgotado',
+}
+
+export default STATUS_CODE

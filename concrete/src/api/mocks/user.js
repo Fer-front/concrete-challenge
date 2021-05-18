826db5af0b0be28a5da39dc2b135767f2eba55b0
fer-front <fer.j.doc@gmail.com>
2021-05-18 15:21:52 -0300

    Update de url de avatar

--- concrete/src/api/mocks/user.js
+++ concrete/src/api/mocks/user.js
@@ -5,7 +5,7 @@ const USERS = [
             name: 'fernando jose',
             nickName: 'Fer-front',
             email: 'fer.j.doc@gmail.com',
-            avatar: 'https://pm1.narvii.com/6362/8fc6948fbb0edacce72bc19ada68ae3f0ea59420_128.jpg',
+            avatar: 'https://a.wattpad.com/useravatar/rachellopesdasilva.256.765766.jpg',
             bio: 'is a web scraping system. It aims to simplify data extraction from the web for UI testing, machine learning, analytics and more.',
             followers: 10,
             following: 200,

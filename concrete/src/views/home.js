39884fa0edc8587201e0a72befb53c78cb0c05a6
fer-front <fer.j.doc@gmail.com>
2021-05-18 19:47:13 -0300

    Update:
     - atualizacao de estilo btnsearch na view home
     - add variavel color-infor
     - estracao de estilo da view home de scss para pasta do compomente

--- concrete/src/views/home.js
+++ concrete/src/views/home.js
@@ -1,4 +1,6 @@
-import React  from 'react'
+import './home.scss'
+
+import React from 'react'
 import URL_ICON from '../constants/icons'
 import debounce from '../utils/debounce'
 import user from '../api/repositories/user-respository'
@@ -15,6 +17,8 @@ class Home extends React.Component {
 
     this.state = {
       usuario: [],
+      loading: false,
+      boxmsg: false,
     }
   }
 
@@ -40,58 +44,72 @@ class Home extends React.Component {
   async searchUserGitHub(userName) {
     const response = await user.getDetail(userName)
     if (response.status === 'error') return []
-  
+
     const dataUser = this.adapterDataListUser(response)
     return [dataUser]
   }
 
   updateList(user) {
-    this.setState({usuario: user})
+    this.setState({ usuario: user })
   }
-  
-  async updateList2(userName) {
+
+  async updateList(userName) {
     const user = await this.searchUserGitHub(userName)
-    this.setState({usuario: user})
+
+    this.setState({
+      ...this.state,
+      usuario: user,
+      loading: false,
+    })
   }
 
   componentDidMount() {
     const inputSearch = document.querySelector("[data-search='user-github']")
     const btnSubmit = document.querySelector('[data-js="submit"]')
 
-    const getUser = debounce(() => (this.updateList2(inputSearch.value)) , 3000)
-    btnSubmit.addEventListener('click', getUser)
+    inputSearch.focus()
+
+    btnSubmit.addEventListener('click', () => {
+      this.setState({ loading: true })
+
+      debounce(() => {
+        this.updateList(inputSearch.value)
+        inputSearch.value = ''
+      }, 1000)()
+    })
   }
 
   render() {
     return (
       <section className="Home container">
-        
+
         <div className="d-flex flex-column justify-content-center align-items-center mb-5 pb-4">
-          <figure className="home__icon-github mx-4 mt-5">
+          <figure className="home__icon-github mx-5 mt-5">
             <img src={URL_ICON.GIT_HUB} alt="icone github" />
           </figure>
 
           <header className="home-header">
             <h1>Buscar usuário</h1>
           </header>
-  
+
           <input
             data-search="user-github"
             type="text"
-            className="home__input-search p-2 text-center border border-success rounded"
+            className="home__input-search p-2 text-center border rounded"
           />
-  
-          <button 
+
+          <button
             data-js="submit"
-            className="mt-4">
-              buscar
+            className="mt-4 g-btn g-btn--primary">
+            buscar
           </button>
-  
+
         </div>
-  
-        <section className="home__list-user">
-          <Loading visible={true} position={'absolute'}></Loading>
-          {this.state.usuario.length ? <ListUser users={this.state.usuario} /> : 'component box-msg implementar'}
+
+        <section className="home__list-user d-flex justify-content-center">
+          <Loading visible={this.state.loading} position={'absolute'}></Loading>
+
+          {this.state.usuario.length ? <ListUser users={this.state.usuario} /> : 'implementar componenteError'}
         </section>
       </section>
     )

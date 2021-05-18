e9857aae65b977b4942dc85673916b8105b615f5
fer-front <fer.j.doc@gmail.com>
2021-05-18 15:37:00 -0300

    add loading e btn busca de usuario

--- concrete/src/views/home.js
+++ concrete/src/views/home.js
@@ -1,41 +1,101 @@
+import React  from 'react'
 import URL_ICON from '../constants/icons'
-
-import ListUser from '../components/user/list-user'
-import { USERS } from '../api/mocks/user.js'
+import debounce from '../utils/debounce'
 import user from '../api/repositories/user-respository'
 
 
+// COMPONENTS 
+// -------------------------------------------------------
+import ListUser from '../components/user/list-user'
+import Loading from '../components/loading/loading'
 
-function Home() {
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
 
-  console.log(user.getDetail('fer-dev'))
+    this.state = {
+      usuario: [],
+    }
+  }
 
+  adapterDataListUser(response) {
+    return {
+      user: {
+        id: response.data.id,
+        name: response.data.login,
+        nickName: response.data.login,
+        email: response.data.email,
+        avatar: response.data.avatar_url,
+        bio: response.data.bio,
+        followers: response.data.followers,
+        following: response.data.following,
+      },
+      respository: {
+        url: response.data.repos_url,
+        total: response.data.public_repos,
+      }
+    }
+  }
 
-  return (
-    <section className="Home container">
+  async searchUserGitHub(userName) {
+    const response = await user.getDetail(userName)
+    if (response.status === 'error') return []
+  
+    const dataUser = this.adapterDataListUser(response)
+    return [dataUser]
+  }
 
-      <div className="d-flex flex-column justify-content-center align-items-center mb-5 pb-4">
-        <figure className="home__icon-github mx-4">
-          <img src={URL_ICON.GIT_HUB} alt="icone github" />
-        </figure>
+  updateList(user) {
+    this.setState({usuario: user})
+  }
+  
+  async updateList2(userName) {
+    const user = await this.searchUserGitHub(userName)
+    this.setState({usuario: user})
+  }
 
-        <header className="home-header">
-          <h1>Buscar usuário</h1>
-        </header>
+  componentDidMount() {
+    const inputSearch = document.querySelector("[data-search='user-github']")
+    const btnSubmit = document.querySelector('[data-js="submit"]')
 
-        <input type="text" className="home__input-search p-2 text-center border border-success rounded"></input>
-      </div>
+    const getUser = debounce(() => (this.updateList2(inputSearch.value)) , 3000)
+    btnSubmit.addEventListener('click', getUser)
+  }
 
-      <section className="home__list-user">
-        <ListUser users={USERS}/>
-      </section>
-
-      <div>
+  render() {
+    return (
+      <section className="Home container">
         
-      </div>
+        <div className="d-flex flex-column justify-content-center align-items-center mb-5 pb-4">
+          <figure className="home__icon-github mx-4 mt-5">
+            <img src={URL_ICON.GIT_HUB} alt="icone github" />
+          </figure>
 
-    </section>
-  );
+          <header className="home-header">
+            <h1>Buscar usuário</h1>
+          </header>
+  
+          <input
+            data-search="user-github"
+            type="text"
+            className="home__input-search p-2 text-center border border-success rounded"
+          />
+  
+          <button 
+            data-js="submit"
+            className="mt-4">
+              buscar
+          </button>
+  
+        </div>
+  
+        <section className="home__list-user">
+          <Loading visible={true} position={'absolute'}></Loading>
+          {this.state.usuario.length ? <ListUser users={this.state.usuario} /> : 'component box-msg implementar'}
+        </section>
+      </section>
+    )
+  }
 }
 
 export default Home;

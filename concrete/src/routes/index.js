5c98ee5a3d75866ebed47901fd00f7a56a28e7a9
fer-front <fer.j.doc@gmail.com>
2021-05-22 22:23:20 -0300

    registrando partial css mediaquery e add comp repository nas rotas

--- concrete/src/routes/index.js
+++ concrete/src/routes/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 // -------------------------------------
 import Home from '../views/home/home'
 import Sobre from '../views/sobre'
+import Repositorios from '../views/repositorios'
 
 export default (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={Home}/>
             <Route path="/Sobre" component={Sobre} />
+            <Route path="/repositorios" component={Repositorios} />
         </Switch>
     </BrowserRouter>
 )

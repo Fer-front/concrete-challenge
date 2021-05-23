42ad2fbf8b7aaceb89a9e85c368a2bcf3eebfc04
fer-front <fer.j.doc@gmail.com>
2021-05-22 22:18:18 -0300

    add component view repository

---
+++ concrete/src/views/repositorios/index.js
@@ -0,0 +1,14 @@
+function repositorios () {
+    return (
+        <section className="list-repository">
+            <div className="container">
+                <h1>Listar repositorios</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nemo tempora pariatur laborum fugit deserunt quisquam distinctio ratione temporibus cumque ducimus officiis ab in, error consectetur nisi! Esse, repellendus ipsum?
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default repositorios

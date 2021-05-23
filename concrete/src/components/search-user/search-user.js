d8056fdeaafcf3cefd508048df16624c056269d3
fer-front <fer.j.doc@gmail.com>
2021-05-22 23:49:03 -0300

    remocao img nao utilizadas, movendo search-user para pasta compoments

---
+++ concrete/src/components/search-user/search-user.js
@@ -0,0 +1,36 @@
+import './search-user.scss'
+
+import URL_ICON from '../../constants/icons'
+
+import Button from '../button/button'
+
+function SearchUser (props) {
+    return (
+        <section className="search-user d-flex justify-content-center flex-column align-items-center">
+            <figure className="search-user__icon-github">
+                <img src={URL_ICON.GIT_HUB} alt="icone github" />
+            </figure>
+
+            <header className="search-user-header">
+                <h1 className="search-user__title">Buscar usuário</h1>
+            </header>
+
+            <input
+                onChange={props.inputChange}
+                type="text"
+                className="search-user__input-search p-2 text-center border rounded"
+            />
+
+            <div className="mt-4">
+                <Button
+                    label="buscar"
+                    type='success'
+                    outline='true'
+                    onClick={props.btnSubmit}
+                />
+          </div>
+        </section>
+    )
+}
+
+export default SearchUser

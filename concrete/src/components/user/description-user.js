e4759e2dc13bfd3c9670637fcaeaf15591ce6823
fer-front <fer.j.doc@gmail.com>
2021-05-25 12:42:49 -0300

    update layout description user

--- concrete/src/components/user/description-user.js
+++ concrete/src/components/user/description-user.js
@@ -1,59 +1,54 @@
-import './description-user.scss'
+import "./description-user.scss";
 
-import Avatar from '../../components/avatar/avatar'
-
-import { GoRepo, GoPerson } from 'react-icons/go'
+import Avatar from "../../components/avatar/avatar";
 
 function descriptionUser(props) {
-    return (
-        <article className="description-user">
-            <div className="d-flex justify-content-center align-items-center mb-4">
-                <Avatar 
-                    className='description-user__avatar'
-                    url={props.data.user.avatar} 
-                    size='xlarge' 
-                    alt={props.data.user.nickName}
-                />
-            </div>
+  if (props.data === undefined || props.data === null) return null;
 
-            <header className="description-user__content mb-3">
-                <h2 className="description-user__name titulo titulo--small text-capitalize font--bold mb-0">
-                    {props.data.user.nickName}
-                </h2>
-                <span className="description-user__email">
-                    {props.data.user.email}
-                </span>
-            </header>
-            <div className="description-user__data">
-                <div className="description-user__bio">
-                    <small className="description-user__bio__label">Bio:</small>
+  return (
+    <article className="description-user">
+      <div className="d-flex justify-content-center align-items-center mb-4">
+        <Avatar
+          className="description-user__avatar"
+          url={props.data.user.avatar}
+          size="xlarge"
+          alt={props.data.user.nickName}
+        />
+      </div>
 
-                    <p className="description-user__bio__text font--light font--error">
-                        {props.data.user.bio}
-                    </p>
-                </div>
-            </div>
-            <div className="my-2 mb-3">
-                <ul className="list-unstyled d-flex align-items-center justify-content-between">
-                    <li className="description-user__data__item mx-2">
-                        <strong className="description-user__data__item-qtd">{props.data.user.followers}</strong> - seguidores
-                    </li>
-                    <li className="description-user__data__item mx-2">
-                        <strong className="description-user__item-qtd">{props.data.user.following}</strong> - seguindo
-                    </li>
-                </ul>
-            </div>
+      <header className="description-user__content mb-3">
+        <h2 className="description-user__name titulo titulo--small text-capitalize font--bold mb-0">
+          {props.data.user.nickName}
+        </h2>
+        <span className="description-user__email">{props.data.user.email}</span>
+      </header>
+      <div className="description-user__data">
+        <div className="description-user__bio">
+          <small className="description-user__bio__label">Bio:</small>
 
-            <div className="d-flex justify-content-between mt-4">
-                <a href={props.data.user.perfil} target="_blank">
-                    <GoPerson className="home__icon home__icon--person" />
-                </a>
-                <a href="/repositorios">
-                    <GoRepo className="home__icon home__icon--repository" />
-                </a>
-            </div>
-        </article>
-    )
+          <p className="description-user__bio__text font--light font--error">
+            {props.data.user.bio}
+          </p>
+        </div>
+      </div>
+      <div className="my-2 mb-3">
+        <ul className="list-unstyled d-flex align-items-center justify-content-between">
+          <li className="description-user__data__item mx-2">
+            <strong className="description-user__data__item-qtd">
+              {props.data.user.followers}
+            </strong>{" "}
+            - seguidores
+          </li>
+          <li className="description-user__data__item mx-2">
+            <strong className="description-user__item-qtd">
+              {props.data.user.following}
+            </strong>{" "}
+            - seguindo
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
 }
 
-export default descriptionUser
+export default descriptionUser;

ae97bea2add4ddb5f9ffeb96fb9bf157ece37d29
fer-front <fer.j.doc@gmail.com>
2021-05-25 00:22:44 -0300

    nova estrutura de markup

--- concrete/src/components/user/description-user.js
+++ concrete/src/components/user/description-user.js
@@ -2,46 +2,29 @@ import './description-user.scss'
 
 import Avatar from '../../components/avatar/avatar'
 
-import { BsPersonFill, BsWallet } from 'react-icons/bs'
+import { GoRepo, GoPerson } from 'react-icons/go'
 
 function descriptionUser(props) {
     return (
-        <article className="description-user mb-5">
-            <div className="d-flex align-items-center mb-2">
-                <div className="mr-3 mb-2">
-                    <Avatar 
-                        className='description-user__avatar'
-                        url={props.data.user.avatar} 
-                        size='large' 
-                        alt={props.data.user.nickName}
-                    />
-                </div>
-
-                <header className="description-user__content">
-                    <h2 className="description-user__name titulo titulo--small text-uppercase mb-0">
-                        {props.data.user.nickName}
-                    </h2>
-                    <span className="description-user__email">
-                        <small>Email:</small> {props.data.user.email}
-                    </span>
-                </header>
+        <article className="description-user">
+            <div className="d-flex justify-content-center align-items-center mb-4">
+                <Avatar 
+                    className='description-user__avatar'
+                    url={props.data.user.avatar} 
+                    size='xlarge' 
+                    alt={props.data.user.nickName}
+                />
             </div>
 
+            <header className="description-user__content mb-3">
+                <h2 className="description-user__name titulo titulo--small text-capitalize font--bold mb-0">
+                    {props.data.user.nickName}
+                </h2>
+                <span className="description-user__email">
+                    {props.data.user.email}
+                </span>
+            </header>
             <div className="description-user__data">
-                <div className="my-2 mb-3">
-                    <ul className="list-unstyled d-flex align-items-center justify-content-center">
-                        <li className="description-user__data__item mx-2">
-                            <strong className="description-user__data__item-qtd mr-1">{props.data.user.followers}</strong> - seguidores
-                        </li>
-                        <li className="description-user__data__item mx-2">
-                            <strong className="description-user__item-qtd mr-1">{props.data.user.following}</strong> - seguindo
-                        </li>
-                        <li className="description-user__data__item mx-2">
-                            <strong className="description-user__item-qtd mr-1">{props.data.user.followers}</strong> - starts
-                        </li>
-                    </ul>
-                </div>
-
                 <div className="description-user__bio">
                     <small className="description-user__bio__label">Bio:</small>
 
@@ -49,23 +32,25 @@ function descriptionUser(props) {
                         {props.data.user.bio}
                     </p>
                 </div>
+            </div>
+            <div className="my-2 mb-3">
+                <ul className="list-unstyled d-flex align-items-center justify-content-between">
+                    <li className="description-user__data__item mx-2">
+                        <strong className="description-user__data__item-qtd">{props.data.user.followers}</strong> - seguidores
+                    </li>
+                    <li className="description-user__data__item mx-2">
+                        <strong className="description-user__item-qtd">{props.data.user.following}</strong> - seguindo
+                    </li>
+                </ul>
+            </div>
 
-                <div className="d-flex flex-wrap justify-content-between">
-                    <div className="description-user__links">
-                        <a href="/" className="d-flex align-items-center">
-                            <BsPersonFill className="mr-2"/> 
-                            Perfil
-                        </a>
-                    </div>
-
-                    <div className="description-user__links">
-                        <a href="/repositorios" className="d-flex align-items-center">
-                            <span className="mr-1">{props.data.respository.total}</span>
-                            <BsWallet className="mr-2"/> 
-                            Repositório
-                        </a>
-                    </div>
-                </div>
+            <div className="d-flex justify-content-between mt-4">
+                <a href={props.data.user.perfil} target="_blank">
+                    <GoPerson className="home__icon home__icon--person" />
+                </a>
+                <a href="/repositorios">
+                    <GoRepo className="home__icon home__icon--repository" />
+                </a>
             </div>
         </article>
     )

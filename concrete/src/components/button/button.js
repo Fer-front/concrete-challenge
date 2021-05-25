eb1742221855da829dbb79740015326f1adc47d4
fer-front <fer.j.doc@gmail.com>
2021-05-25 18:44:00 -0300

    add opcao de tamanho no componente button

--- concrete/src/components/button/button.js
+++ concrete/src/components/button/button.js
@@ -1,30 +1,28 @@
-import './button.scss'
+import "./button.scss";
 
 const MAP_TYPES = {
-    primary: '--primary',
-    success: '--success',
-    error: '--error',
-    infor: '--infor',
-    alert: '--alert',
-    disabled: '--disabled',
+  primary: "--primary",
+  success: "--success",
+  error: "--error",
+  infor: "--infor",
+  alert: "--alert",
+  disabled: "--disabled",
+};
+
+function composeClassCss(type = false, outline = false, size = 'default') {
+  const _type = MAP_TYPES[type] || "";
+
+  return outline ? `g-btn g-btn--${size} g-btn__outline${_type} ` : `g-btn g-btn--${size} g-btn${_type}`;
 }
 
-function composeClassCss(type = false, outline = false) {
-    const _type = MAP_TYPES[type] || ''
+function Button(props) {
+  const _class = composeClassCss(props.type, props.outline, props.size);
 
-    return outline 
-        ? `g-btn g-btn__outline${_type}`
-        : `g-btn g-btn${_type}` 
+  return (
+    <button onClick={() => props.onClick()} className={_class}>
+      {props.icon} {props.label}
+    </button>
+  );
 }
 
-function Button (props) {
-    const _class = composeClassCss(props.type, props.outline)
-
-    return (
-        <button 
-            onClick={()=> (props.onClick())}
-            className={_class}>{props.label}</button>
-    )
-}
-
-export default Button
+export default Button;

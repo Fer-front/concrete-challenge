4c6a70acf84fa2c9e4ae851e2f737110d7fffa14
fer-front <fer.j.doc@gmail.com>
2021-05-25 00:19:28 -0300

    ajuste de layout

--- concrete/src/components/search-user/search-user.js
+++ concrete/src/components/search-user/search-user.js
@@ -1,31 +1,62 @@
 import './search-user.scss'
 
-import URL_ICON from '../../constants/icons'
-
 import Button from '../button/button'
+
+// utils
+// ---------------------------------------
+import random from '../../utils/random'
+
+// icons
+// ---------------------------------------
+import octcat from '../../assets/icon-ocat.png'
+import octcat_yoga from '../../assets/icon-ocat-yoga.gif'
+import octcat_disco from '../../assets/icon-ocat-disco.gif'
+import octcat_jet from '../../assets/icon-octa-jet.png'
+import octcat_shinoby from '../../assets/icon-octa-shinoby.jpg'
+import octcat_white from '../../assets/icon-octa-white.png'
+import octcat_wolv from '../../assets/icon-octa-wolv.jpg'
+ 
+
+
+function randomIcon() {
+    const icons = [
+        octcat, 
+        octcat_disco, 
+        octcat_yoga,
+        octcat_jet,
+        octcat_shinoby,
+        octcat_white,
+        octcat_wolv,
+    ]
+
+    const len = icons.length
+
+    const index = random(0, len)
+    
+    return icons[index]
+}
+
+const dinamicIcon = randomIcon()
 
 function SearchUser (props) {
     return (
         <section className="search-user d-flex justify-content-center flex-column align-items-center">
-            <figure className="search-user__icon-github">
-                <img src={URL_ICON.GIT_HUB} alt="icone github" />
+            
+            <figure className="search-user__icon-github d-flex align-items-end">
+                <img src={dinamicIcon} alt="icone github" />
             </figure>
 
-            <header className="search-user-header">
-                <h1 className="search-user__title">Buscar usuário</h1>
-            </header>
-
             <input
+                className="search-user__input-search p-2 border text-center"
                 onChange={props.inputChange}
-                type="text"
-                className="search-user__input-search p-2 text-center border rounded"
+                type="search"
+                placeholder="Buscar usuário"
             />
 
             <div className="mt-4">
                 <Button
                     label="buscar"
-                    type='success'
-                    outline='true'
+                    type='primary'
                     onClick={props.btnSubmit}
                 />
           </div>

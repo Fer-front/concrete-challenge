1164aee9e32ebe28c5b54494114960f79781e5d1
fer-front <fer.j.doc@gmail.com>
2021-05-26 17:20:11 -0300

    remocao da rota sobre

--- concrete/src/routes/index.js
+++ concrete/src/routes/index.js
@@ -3,14 +3,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 // views
 // -------------------------------------
 import Home from '../views/home/home'
-import Sobre from '../views/sobre'
 import Repositorios from '../views/repositorios'
 
 export default (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={Home}/>
-            <Route path="/Sobre" component={Sobre} />
             <Route path="/repositorios" component={Repositorios} />
         </Switch>
     </BrowserRouter>

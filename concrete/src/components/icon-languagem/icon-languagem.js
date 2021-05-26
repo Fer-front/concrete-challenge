93c424f1013172d80c7950ebba813d5e3ecdbc1b
fer-front <fer.j.doc@gmail.com>
2021-05-26 17:12:38 -0300

    add novos icones e regra para icone default

--- concrete/src/components/icon-languagem/icon-languagem.js
+++ concrete/src/components/icon-languagem/icon-languagem.js
@@ -1,9 +1,46 @@
 import "./icon-languagem.scss";
 
 export default function iconLang(props) {
+  const icon_url = [
+    'angular',
+    'babel',
+    'clojure',
+    'cobol',
+    'dart',
+    'elixir',
+    'elm',
+    'erlang',
+    'go',
+    'graphql',
+    'grunt',
+    'handlebars',
+    'haskell',
+    'html',
+    'java',
+    'javascript',
+    'typescript',
+    'swift',
+    'vue',
+    'react',
+    'node',
+    'json',
+    'sass',
+    'scss',
+    'shell',
+    'less',
+    'python',
+    'kotlin',
+    'ruby',
+    'docker',
+    'dockerfile',
+    'csharp',
+  ];
+
+  const _type = props.type.toLowerCase().trim().replace(/\s/g, "_");
+  const type = icon_url.includes(_type) ? _type : "shell";
+
   const size = props.size || "30";
-  const type = props.type || "javascript";
 
   const _class = `icon-lang icon-lang--${type} icon-lang--${size}`;
-  return <div className={_class}></div>;
+  return <div className={_class} title={type}></div>;
 }
